740044d948a71eaad00c19bd529e0b8beda474e3
xmichelle26x <xmichelle26x@gmail.com>
2023-12-09 20:42:41 -0500

    del not necessary files

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,30 +1,23 @@
-// main_test.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weatherapp/main.dart';
 
 void main() {
-  testWidgets('Weather App UI Test', (WidgetTester tester) async {
+  testWidgets('Weather app UI test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
-    // Verify the initial UI state.
+    // Verify if the initial UI elements are present.
     expect(find.text('Weather App'), findsOneWidget);
     expect(find.text('Enter city name'), findsOneWidget);
-    expect(find.text('Get Weather'), findsOneWidget);
-    expect(find.text('City:'), findsNothing);
-    expect(find.text('Temperature:'), findsNothing);
-    expect(find.text('Weather Condition:'), findsNothing);
+    expect(find.byType(ElevatedButton), findsOneWidget);
 
-    // Simulate user input and tap the button.
-    await tester.enterText(find.byType(TextField), 'London');
+    // Tap the "Get Weather" button and wait for the response.
     await tester.tap(find.byType(ElevatedButton));
     await tester.pump();
 
-    // Verify the updated UI state after fetching weather data.
-    expect(find.text('City: London'), findsOneWidget);
-    expect(find.text('Temperature:'), findsOneWidget);
-    expect(find.text('Weather Condition:'), findsOneWidget);
+    // Verify if the city error message is displayed.
+    expect(find.text('City not found. Please enter a valid city name.'), findsOneWidget);
   });
 }
+
